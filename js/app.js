ca9ec7d728d0be3e19b34c646925c03d4ff8c5bc
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-11-23 09:35:08 +0100

    Adding the first check function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
+function isDayFieldValid() {
+  const $dayInputField = document.querySelector('#day')
+  const dayInputValue = $dayInputField.value
+
+  const isInputInvalid = isNaN(dayInputValue) || dayInputValue <= 0 || dayInputValue > 31
+
+  if (isInputInvalid) {
+    console.log('invalid')
+  } else {
+    console.log('valid')
+  }
+}
+
+function onSubmitForm() {
+  const $form = document.querySelector('.form')
+
+  $form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    isDayFieldValid()
+  })
+}
+
 function main() {
-  console.log("Hello, World!")
+  onSubmitForm()
 }
 
 main()
