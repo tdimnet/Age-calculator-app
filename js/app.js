e94a2a17c77d363f143ea618491fe76afa1b7b25
tdimnet <thomas.dimnet@gmail.com>
2023-11-24 16:31:06 +0100

    Updating how error messages are displayed
    - Adding new params to displayErrorMessage
    - Returning a boolean

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 /**
  * 
  * @param {Element} domNode 
+ * @param {string} errorMsg
+ * 
+ * @returns {boolean}
  */
-function displayErrorMessage(domNode) {
+function displayErrorMessage(domNode, errorMsg) {
   domNode.parentElement.classList.add('has-error')
   domNode.nextElementSibling.classList.remove('hidden')
+  domNode.nextElementSibling.textContent = errorMsg
 }
 
 function isDayFieldValid() {
+  let isValid = Boolean
+
   const $dayInputField = document.querySelector("#day");
   const dayInputValue = $dayInputField.value;
 
@@ -15,10 +21,14 @@ function isDayFieldValid() {
     isNaN(dayInputValue) || dayInputValue <= 0 || dayInputValue > 31;
 
   if (isInputInvalid) {
-    displayErrorMessage($dayInputField)
+    displayErrorMessage($dayInputField, 'Must be a valid day.')
+    isValid = false
   } else {
     console.log("valid");
+    isValid = true
   }
+
+  return isValid
 }
 
 function onSubmitForm() {
@@ -27,7 +37,7 @@ function onSubmitForm() {
   $form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    isDayFieldValid();
+    const isDayValid = isDayFieldValid();
   });
 }
 
